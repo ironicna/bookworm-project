6c9037bd692a0f08afbf4811ea8dbd9c1ceea3a2
Lamija <ironicna@gmail.com>
2020-05-27 13:44:33 +0200

    fixed database connection

--- server/index.js
+++ server/index.js
@@ -10,17 +10,19 @@ const config = require("./config/key");
 
  const mongoose = require("mongoose");
  mongoose
-   .connect(config.mongoURI, { useNewUrlParser: true })
+   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => console.log("DB connected"))
    .catch(err => console.error(err));
 
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
-  })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// const connect = mongoose.connect(config.mongoURI,
+//   {
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true,
+//     useCreateIndex: true, 
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
 
 app.use(cors())
 
